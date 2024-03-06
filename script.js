e2674c9864a0c53d1ad862c884424fd7bfa2d78a
Vedika Mishra <151787416+vedikamishragithub@users.noreply.github.com>
2024-03-07 02:45:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const makeChange = (c) => {
-  // your name here
+    const coins = {
+        'q': 25, 
+        'd': 10,
+        'n': 5, 
+        'p': 1,
+    };
+    let result = {
+        'q': 0,
+        'd': 0,
+        'n': 0,
+        'p': 0,
+    };
+ 
+    for (let coin in coins) {
+        let div = Math.floor(c / coins[coin]);
+        result[coin] = div;
+        c -= div * coins[coin];
+    }
+    return result;
 };
 
 // Do not the change the code below
